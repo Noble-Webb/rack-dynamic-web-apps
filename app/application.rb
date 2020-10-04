e3df3eb6e49c5468fca988565a5137ae954bd12e
Justin_Webb <Webb.n.justin@gmail.com>
2020-10-04 13:50:14 -0600

    created a slot game using CLI apps as a framework

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,27 @@
 class Application
 
-  def call(env)
+  def call(env) #calls the environment?
     resp = Rack::Response.new
-    resp.write "Hello, World"
+
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+    #slot game will need to randomize numbers to function
+
+    resp.write "#{num_1}\n"
+    resp.write "#{num_2}\n"
+    resp.write "#{num_3}\n"
+    #makes the randomized nums visible to the user with new lines separating them
+
+    if num_1==num_2 && num_2==num_3 
+      #if all of the numbers match
+      resp.write "You Win"
+      #must use write not puts for html
+    else 
+      resp.write "You Lose"
+      #unless the nums match you lose
+    end
+
     resp.finish
   end
 
